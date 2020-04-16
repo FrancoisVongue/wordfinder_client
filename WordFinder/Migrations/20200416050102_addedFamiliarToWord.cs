2d5094baace10a53b6a3402d1301647b3ff3391a
Francois Vongue <francoisvongue@gmail.com>
2020-04-16 11:16:41 +0300

    added proper pagination

---
+++ WordFinder/Migrations/20200416050102_addedFamiliarToWord.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WordFinder.Migrations
+{
+    public partial class addedFamiliarToWord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Familiar",
+                table: "Words",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Familiar",
+                table: "Words");
+        }
+    }
+}
